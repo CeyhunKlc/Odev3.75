50e046031ae1cc5ccda8dfed8d55f30734c8e64a
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-11-25 11:06:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace odev3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1. Sayiyi Giriniz:");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. Sayiyi Giriniz:");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 <= sayi2)
+            {
+                for (int i = sayi1; i <= sayi2; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            else
+            {
+
+
+                for (int i = sayi1; i >= sayi2; i--)
+                {
+
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
